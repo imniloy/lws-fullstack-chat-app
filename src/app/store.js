d6ce5d0bec33967340eac4d6456468f851e2ee49
imniloy <imniloy8@gmail.com>
2022-11-20 20:03:13 +0600

    ....

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import apiSlice from '../features/api/apiSlice';
-import authSliceReducer from '../features/auth/authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import apiSlice from "../features/api/apiSlice";
+import authSliceReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer || {},
     auth: authSliceReducer,
   },
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
